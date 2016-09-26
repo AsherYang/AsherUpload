78ab5a804963be1037f8416bc6c8a84a6b44efc9
ouyangfan <ouyangfan1991@gmail.com>
2016-09-26 23:57:13 +0800

    run python script

--- src/com/asher/yang/upload/form/ConfigurationPanel.java
+++ src/com/asher/yang/upload/form/ConfigurationPanel.java
@@ -3,6 +3,9 @@ package com.asher.yang.upload.form;
 import com.intellij.openapi.project.Project;
 
 import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 /**
  * Created by AsherYang on 2016/9/24.
@@ -22,7 +25,28 @@ public class ConfigurationPanel {
 
     public ConfigurationPanel(Project project) {
         this.project = project;
+
+        testConnectionButton.addActionListener(mConnectBtnActionListener);
     }
+
+    private ActionListener mConnectBtnActionListener = e -> {
+        try {
+            Process process = Runtime.getRuntime().exec("python /Users/ouyangfan/Documents/ouyangfan/code_program/python_pro/hello.py");
+            InputStreamReader isr = new InputStreamReader(process.getInputStream());
+            BufferedReader br = new BufferedReader(isr);
+
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                System.out.println("line = " + line);
+            }
+
+            br.close();
+            isr.close();
+            process.waitFor();
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+    };
 
     public JPanel getRootPanel() {
         return rootPanel;
